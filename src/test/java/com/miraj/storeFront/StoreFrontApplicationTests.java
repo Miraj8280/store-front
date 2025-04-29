d6cb3176cea2fb3256235614b9cfaa51278d3ab8
Miraj Asraf <mirajasraf786@gmail.com>
2025-04-29 21:44:05 +0530

    Project setup

---
+++ src/test/java/com/miraj/storeFront/StoreFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraj.storeFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
